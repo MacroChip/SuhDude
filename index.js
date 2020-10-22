75ab8b79b36df15bc6b95935f2b52b9f5a1c1f19
Chip Thien <themacrochip@gmail.com>
2020-10-22 01:52:28 -0500

    Gui can insert configs into database

--- index.js
+++ index.js
@@ -14,16 +14,40 @@ if (!process.env.PASSWORD) {
     throw "Need to provide a password in PASSWORD environment variable";
 }
 
-router.get('/', ctx => {
-    ctx.body = 'Hello Koa';
+const db = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
+
+db.connect();
+
+db.query(`CREATE TABLE IF NOT EXISTS config (
+    id varchar(64) PRIMARY KEY,
+    name varchar(64) NOT NULL,
+    url varchar(200) NOT NULL
+)`)
+    .then(res => console.log(`Made or found config table`))
+    .catch(err => console.log(`Error making config table`, err));
 
 router.post('/change', ctx => {
     const body = ctx.request.body;
     if (body.word === process.env.PASSWORD) {
         console.log(`Password correct:`, body);
-        ctx.response.status = 200;
-        ctx.body = `${body.name} (ID ${body.id}) changed to ${body.url}`;
+        db.query('INSERT INTO config VALUES($1, $2, $3);', [body.id, body.name, body.url])
+            .then(res => {
+                for (let row of res.rows) {
+                    console.log(JSON.stringify(row));
+                }
+                ctx.response.status = 200;
+                ctx.body = `${body.name} (ID ${body.id}) changed to ${body.url}`;
+            })
+            .catch(err => {
+                console.log(`Error making change`, err);
+                ctx.body = `Error`;
+                ctx.response.status = 500;
+            });
     } else {
         console.log(`Failed password attempt. User attempted with password: ${body.word}`);
         ctx.response.body = `wrong secret word`;
