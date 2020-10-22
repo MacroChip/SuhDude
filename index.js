fd905119eee6980906ee463521e6637000741337
Chip Thien <themacrochip@gmail.com>
2020-10-22 01:31:19 -0500

    Add simple password auth

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const Router = require('koa-router');
 const koaStatic = require('koa-static');
+require('dotenv').config();
 const join = require('path').join;
 const cors = require('@koa/cors');
 const mount = require('koa-mount');
@@ -9,14 +10,25 @@ const bodyparser = require('koa-bodyparser');
 const app = new Koa();
 const router = new Router();
 
+if (!process.env.PASSWORD) {
+    throw "Need to provide a password in PASSWORD environment variable";
+}
+
 router.get('/', ctx => {
     ctx.body = 'Hello Koa';
 });
 
 router.post('/change', ctx => {
     const body = ctx.request.body;
-    ctx.response.code = 200;
-    ctx.body = `${body.name} (ID ${body.id}) changed to ${body.url}`;
+    if (body.word === process.env.PASSWORD) {
+        console.log(`Password correct:`, body);
+        ctx.response.status = 200;
+        ctx.body = `${body.name} (ID ${body.id}) changed to ${body.url}`;
+    } else {
+        console.log(`Failed password attempt. User attempted with password: ${body.word}`);
+        ctx.response.body = `wrong secret word`;
+        ctx.response.status = 403;
+    }
 });
 
 const apiRoute = '/api'
